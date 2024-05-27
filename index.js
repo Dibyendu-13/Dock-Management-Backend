9e7dd28e4506d8d5e59b4e46b2862218836a9bde
Dibyendu <dibyendubar13702@gmail.com>
2024-05-28 03:08:37 +0530

    prod

--- index.js
+++ index.js
@@ -11,16 +11,18 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'https://dock-mgmt.netlify.app/',
+    origin: 'https://dock-mgmt.netlify.app',
     methods: ["GET", "POST"]
   }
 });
 
-
-const PORT =process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://dock-mgmt.netlify.app',
+  methods: ["GET", "POST"]
+}));
 
 let docks = Array.from({ length: 10 }, (_, i) => ({
   dockNumber: i + 1,
